936c6cd38cea6635cad9e18fd51b40bbb7c0a7f1
NicknEma <emanuele.rovini@gmail.com>
2026-01-05 16:36:54 +0100

    Add icon

--- build.bat
+++ build.bat
@@ -1,4 +1,12 @@
 @echo off
-del *.pdb > NUL 2> NUL
+
 if not exist build mkdir build
-odin build source -out:build/whitespaze_debug.exe -debug -vet-shadowing
+pushd build
+
+del *.pdb > NUL 2> NUL
+
+rc /nologo /foresources.res ../source/resources.rc
+odin build ../source -out:whitespaze_debug.exe -debug -vet-shadowing -warnings-as-errors -extra-linker-flags:resources.res
+del resources.res > NUL 2> NUL
+
+popd
